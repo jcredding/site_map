49f764001c6f23fefed7f88c74d258040e9bafc7
jcredding <TempestTTU@gmail.com>
2010-01-13 13:37:58 -0600

    basic gem setup

---
+++ test/unit/site_map_test.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class SiteMapTest < Test::Unit::TestCase
+
+  context "An instance of the SiteMap class" do
+
+    should "flunk" do
+      flunk "Please provide some tests"
+    end
+    
+  end
+
+end
